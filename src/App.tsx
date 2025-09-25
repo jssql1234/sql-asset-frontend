94c43d0a6f3d1ca354517b38999990681c32e5c9
ongth-68 <154893957+ongth-68@users.noreply.github.com>
2025-09-25 17:59:03 +0800

    fix: Commented example translations in App.tsx to use default text for table

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,32 @@ import AppRoutes from "@/routes";
 import { ToastProvider } from "./components/ui/components/Toast";
 import { ThemeProvider } from "./context/ThemeContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 import { TranslationProvider } from "./components/ui/components/Table";
 
 const queryClient = new QueryClient();
 
 function App() {
-  const { t } = useTranslation("pagination");
+  // const { t } = useTranslation("pagination");
 
-  const tableTranslations = {
-    rowsSelected: (selected: number, total: number) =>
-      t("rowsSelected", { selected, total }),
-    rowsPerPage: t("rowsPerPage"),
-    pageOf: (current: number, total: number) => t("pageOf", { current, total }),
-    firstPage: t("firstPage"),
-    previousPage: t("previousPage"),
-    nextPage: t("nextPage"),
-    lastPage: t("lastPage"),
-  };
+  // const tableTranslations = {
+  //   rowsSelected: (selected: number, total: number) =>
+  //     t("rowsSelected", { selected, total }),
+  //   rowsPerPage: t("rowsPerPage"),
+  //   pageOf: (current: number, total: number) => t("pageOf", { current, total }),
+  //   firstPage: t("firstPage"),
+  //   previousPage: t("previousPage"),
+  //   nextPage: t("nextPage"),
+  //   lastPage: t("lastPage"),
+  // };
 
   return (
     <ThemeProvider>
       <ToastProvider>
         <BrowserRouter>
           <QueryClientProvider client={queryClient}>
-            <TranslationProvider translations={tableTranslations}>
+            {/* <TranslationProvider translations={tableTranslations}> */}
+            <TranslationProvider>
               <AppRoutes />
             </TranslationProvider>
           </QueryClientProvider>

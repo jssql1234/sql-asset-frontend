5e5255ba97ab203fcd26a0dae0205000a1328a49
NathanielCJX <nathanielchai412@gmail.com>
2025-10-09 12:38:30 +0800

    feat: SidebarHeader stick on top

--- src/layout/sidebar/SidebarHeader.tsx
+++ src/layout/sidebar/SidebarHeader.tsx
@@ -14,7 +14,7 @@ export function SidebarHeader({ children, breadcrumbs }: SidebarHeaderProps) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <header className="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b bg-background transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex flex-1 items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <SidebarSeparator
